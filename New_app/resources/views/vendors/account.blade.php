4604088325212e005a5c0291248562934ec1ba2f
IreshaBandaranayake <ireshabke@gmail.com>
2020-12-09 22:56:28 +0530

    User Accounts - Layouts

---
+++ New_app/resources/views/vendors/account.blade.php
@@ -0,0 +1,9 @@
+@extends('frontEnd.layouts.master')
+@section('title','My Account Page')
+@section('slider')
+@endsection
+@section('content')
+    
+<h1>vendors dashboard</h2>
+    <div style="margin-bottom: 20px;"></div>
+@endsection
