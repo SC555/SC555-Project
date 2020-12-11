1d85c110e6d6c4446f6207b719b2f8ea61b256db
charunika <charu14.d@gmail.com>
2020-12-11 12:05:52 +0530

    modified backend dashboard for seller compatibility

--- New_app/resources/views/backEnd/layouts/header.blade.php
+++ New_app/resources/views/backEnd/layouts/header.blade.php
@@ -1,28 +1,23 @@
 <!--Header-part-->
 <div id="header">
+  @if(Session::has('vendorUser')) 
+	<h1><a href="{{url('/admin')}}">E Mart</a></h1>
+   
+	 @else
 <h1><a href="{{url('/admin')}}">E Mart</a></h1>
     <p> Admin </p>
+	@endif
 </div>
 <!--close-Header-part-->
 <!--top-Header-menu-->
 <div id="user-nav" class="navbar navbar-inverse">
     <ul class="nav">
-    
-        
-    @if(Session::has('vendorUser')) 
-    <li class=""></li>
-   
-    <li><a href="{{ url('/logout_vendor') }}"><i class="icon icon-signout"></i> Logout </a></li>
-
-    @else
-    <li class=""><a title="" href="{{url('/admin/settings')}}"><i class="icon icon-cog"></i> <span class="text">Settings</span></a></li>
-  
-    
+	 @if(Session::has('vendorUser')) 
         <li class="">
             <a class="dropdown-item" href="{{ route('logout') }}"
-               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-               <i class="icon icon-signout"></i>{{ __('Logout') }}
+               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+			   
+                <i class="icon icon-signout"> </i>{{ __('Logout') }}
             </a>
 
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -30,13 +25,26 @@
             </form>
 
         </li>
-     @endif
-     <li class=""><a title="" href=""><i class="icon-user"></i> <span class="text"></span></a></li>
-    <li class=""><a title="" href=""><i class="icon-bell"></i><span class="text"></span></a></li>
-    <li class=""><a href="#"><span class="text"> Hello {{{ isset(Auth::user()->name) ? Auth::user()->name : Auth::user()->email }}}</a></span></li>
+		<li class=""><a href="#"><span class="text"> Hello {{{ isset(Auth::user()->name) ? Auth::user()->name : Auth::user()->email }}}</a></span></li>
+		  @else
+		  <li class="">
+            <a class="dropdown-item" href="{{ route('logout') }}"
+               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+			   
+                <i class="icon icon-signout"> </i>{{ __('Logout') }}
+            </a>
 
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
 
-    </ul>
+        </li>
+        <li class=""><a title="" href=""><i class="icon-user"></i> <span class="text"></span></a></li>
+        <li class=""><a title="" href=""><i class="icon-bell"></i><span class="text"></span></a></li>
+        <li class=""><a title="" href="{{url('/admin/settings')}}"><i class="icon icon-cogs"></i> <span class="text"></span></a></li>
+		<li class=""><a href="#"><span class="text"> Hello {{{ isset(Auth::user()->name) ? Auth::user()->name : Auth::user()->email }}}</a></span></li>
+   @endif
+   </ul>
 </div>
 <!--close-top-Header-menu-->
 <!--start-top-serch-->
